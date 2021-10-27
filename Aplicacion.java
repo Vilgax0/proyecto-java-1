7e2e966ecd7fbbcbea3eccba312a5063af1e9185
Vilgax0 <mkclaren.tobar@upb.edu.co>
2021-10-27 11:35:00 -0500

    Java
    Co-Authored-By: Camilo Guzmán <89235206+axensz@users.noreply.github.com>

---
+++ Aplicacion.java
@@ -0,0 +1,70 @@
+
+import java.util.Scanner;
+
+public class Aplicacion {
+    public static void main(String[]args) {
+
+        Scanner in = new Scanner(System.in);
+
+        do{
+        System.out.println(   "_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._");
+        System.out.println( ",'_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._`.");
+        System.out.println("( (                                                         ) )");
+        System.out.println(" ) )                    BIENVENIDO A TU                    ( (");
+        System.out.println("( (                   CALCULADORA FAVORITA                  ) )");
+        System.out.println(" ) )                                                       ( (");
+        System.out.println("( (         Elije tu Opcion:                                ) )");
+        System.out.println(" ) )                                                       ( (");
+        System.out.println("( (         1. Calcular tu signo del zodiaco                ) )");
+        System.out.println(" ) )        2. Calcular Tus dias vividos.                  ( (");
+        System.out.println("( (         3. Calcular tu edad en años de perro.           ) )");
+        System.out.println(" ) )        4. Caluclar tu número según la numerología.    ( (");
+        System.out.println("( (         5. Calcular todos los anteriores.               ) )");
+        System.out.println(" ) )        6. Salir                                       ( (");
+        System.out.println("( (                                                         ) )");
+        System.out.println(" ) )           Realizada por Mkclaren y Camilo.           ( (");
+        System.out.println("( (_.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._) )");
+        System.out.println(" `._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._,'");
+        System.out.println("");
+        
+        System.out.print("Ingrese una opcion ");
+        int opc = in.nextInt();
+
+
+        if(opc == 1)
+        {
+            //
+        }
+
+        if(opc == 2)
+        {
+            //
+        }
+
+        if(opc == 3)
+        {
+            //
+        }
+
+        if(opc == 4)
+        {
+            //
+        }
+
+        if(opc == 5)
+        {
+            System.out.println("Hasta pronto");
+        }
+
+        if(opc == 6)
+        {
+            System.out.println("Hasta pronto");
+        }
+
+    }while(opc != 6);
+
+
+        
+        
+    } 
+}
